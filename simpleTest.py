ebbbea0d3c1bc05f0d4386a189e69ff578590aa0
Vishal Srinivas Narasimhan <vsn2@illinois.edu>
2020-02-16 21:12:13 -0600

    made robot move through python, now working on sensor input

--- simpleTest.py
+++ simpleTest.py
@@ -8,6 +8,8 @@
 #
 # IMPORTANT: for each successful call to simxStart, there
 # should be a corresponding call to simxFinish at the end!
+def radians(angle):
+    return angle * (math.pi/180)
 
 try:
     import sim
@@ -21,6 +23,7 @@ except:
     print ('')
 
 import time
+import math
 
 print ('Program started')
 sim.simxFinish(-1) # just in case, close all opened connections
@@ -37,7 +40,86 @@ if clientID!=-1:
 
     time.sleep(2)
 
-    # Now retrieve streaming data (i.e. in a non-blocking fashion):
+    #print(objs)
+
+    jointHandles=[-1,-1,-1,-1,-1,-1]
+    for i in range(6):
+       jointHandles[i]=sim.simxGetObjectHandle(clientID, 'UR3_joint' + str(i+1)+'#', sim.simx_opmode_blocking)
+       print(jointHandles[i])
+
+    #Set-up some of the RML vectors:
+    vel=180
+    accel=40
+    jerk=80
+    currentVel=[0,0,0,0,0,0,0]
+    currentAccel=[0,0,0,0,0,0,0]
+    maxVel=[vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180]
+    maxAccel=[accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180]
+    maxJerk=[jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180]
+    targetVel=[0,0,0,0,0,0]
+    
+    targetPos0 = [radians(90),radians(90),radians(-92),radians(233),radians(33),radians(24)]
+    sim.simxPauseCommunication(clientID, True)
+    for i in range(6):
+        print(jointHandles[i])
+        print(targetPos0[i])
+        if(i==0):
+            sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos0[i], sim.simx_opmode_streaming)
+            sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_streaming)
+        else:
+           sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos0[i], sim.simx_opmode_buffer)
+           sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_buffer) 
+
+    sim.simxPauseCommunication(clientID, False)
+    # sim.rmlMoveToJointPositions(jointHandles, -1, currentVel, currentAccel, maxVel, maxAccel, maxJerk, targetPos0, targetVel)
+    
+    targetPos1 = [radians(90),radians(90),radians(-92),radians(233),radians(33),radians(24)]
+    sim.simxPauseCommunication(clientID, True)
+    for i in range(6):
+        print(jointHandles[i])
+        print(targetPos0[i])
+        if(i==0):
+            sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos1[i], sim.simx_opmode_streaming)
+            sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_streaming)
+        else:
+           sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos1[i], sim.simx_opmode_buffer)
+           sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_buffer) 
+
+    sim.simxPauseCommunication(clientID, False)
+    # sim.rmlMoveToJointPositions(jointHandles,-1,currentVel,currentAccel,maxVel,maxAccel,maxJerk,targetPos1,targetVel)
+    time.sleep(1)
+    targetPos2 = [radians(90),radians(90),radians(-92),radians(233),radians(33),radians(24)]
+    sim.simxPauseCommunication(clientID, True)
+    for i in range(6):
+        print(jointHandles[i])
+        print(targetPos0[i])
+        if(i==0):
+            sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos2[i], sim.simx_opmode_streaming)
+            sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_streaming)
+        else:
+           sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos2[i], sim.simx_opmode_buffer)
+           sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_buffer) 
+
+    sim.simxPauseCommunication(clientID, False)
+    # sim.rmlMoveToJointPositions(jointHandles,-1,currentVel,currentAccel,maxVel,maxAccel,maxJerk,targetPos2,targetVel)
+    time.sleep(1)
+    targetPos3=[0,0,0,0,0,0]
+    #targetPos2 = [radians(90),radians(90),radians(-92),radians(233),radians(33),radians(24)]
+    sim.simxPauseCommunication(clientID, True)
+    for i in range(6):
+        print(jointHandles[i])
+        print(targetPos0[i])
+        if(i==0):
+            sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos3[i], sim.simx_opmode_streaming)
+            sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_streaming)
+        else:
+           sim.simxSetJointTargetPosition(clientID, jointHandles[i][1], targetPos3[i], sim.simx_opmode_buffer)
+           sim.simxSetJointTargetVelocity(clientID, jointHandles[i][1], targetVel[i], sim.simx_opmode_buffer) 
+
+    sim.simxPauseCommunication(clientID, False)    
+    time.sleep(1)
+    # sim.rmlMoveToJointPositions(jointHandles,-1,currentVel,currentAccel,maxVel,maxAccel,maxJerk,targetPos3,targetVel)
+    # # Now retrieve streaming data (i.e. in a non-blocking fashion):
     startTime=time.time()
     sim.simxGetIntegerParameter(clientID,sim.sim_intparam_mouse_x,sim.simx_opmode_streaming) # Initialize streaming
     while time.time()-startTime < 5:
